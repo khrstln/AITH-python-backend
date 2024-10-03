b69a24eaf542664b01f6b1cf9f71c3a2b503623f
khrstln <Khrstln@yandex.ru>
2024-10-03 18:16:17 +0300

    Add infrastructure\data

---
+++ HW_2/infrastructure/data/repos/cart_repo/cart_repo.py
@@ -0,0 +1,67 @@
+from typing import Optional, List
+
+from HW_2.core.repos.cart_repo.interface_cart_repo import InterfaceCartRepo
+from HW_2.core.repos.cart_repo.dto.post_cart_dto import PostCartDTO
+from HW_2.core.entities.cart import Cart
+from HW_2.core.entities.item import Item
+
+
+class CartRepo(InterfaceCartRepo):
+    def __init__(self) -> None:
+        self._carts = []
+
+    async def post_cart(self, cart_dto: PostCartDTO) -> int:
+        cart = Cart(id=len(self._carts), items=[], price=0.0)
+        self._carts.append(Cart)
+
+        return cart.id
+
+    async def get_cart_by_id(self, cart_id: int) -> Optional[Cart]:
+        try:
+            cart = self._carts[cart_id]
+            return cart
+        except IndexError:
+            return None
+
+    async def get_carts(self,
+                        offset: Optional[int] = 0,
+                        limit: Optional[int] = 10,
+                        min_price: Optional[float] = None,
+                        max_price: Optional[float] = None,
+                        min_quantity: Optional[int] = None,
+                        max_quantity: Optional[int] = None) -> List[Cart]:
+        carts = self._carts[offset:offset + limit]
+
+        if min_price is not None:
+            carts = [cart for cart in carts if cart.price >= min_price]
+
+        if max_price is not None:
+            carts = [cart for cart in carts if cart.price <= max_price]
+
+        if min_quantity is not None:
+            carts = [cart for cart in self._carts if len(cart.items)
+                     >= min_quantity]
+
+        if max_quantity is not None:
+            carts = [cart for cart in self._carts if len(cart.items)
+                     <= max_quantity]
+
+        return carts
+
+    async def post_item_to_cart(self, items: List[Item],  item_id: int,
+                                cart_id: int) -> Optional[Cart]:
+        try:
+            cart = self._carts[cart_id]
+        except IndexError:
+            return None
+
+        try:
+            new_item = items[item_id]
+        except IndexError:
+            return None
+
+        cart.items.append(new_item)
+
+        cart.price += new_item.price
+
+        return cart
---
+++ HW_2/infrastructure/data/repos/item_repo/item_repo.py
@@ -0,0 +1,87 @@
+from typing import Optional, List
+
+from HW_2.core.repos.item_repo.interface_item_repo import InterfaceItemRepo
+from HW_2.core.repos.item_repo.dto.post_item_dto import PostItemDTO
+from HW_2.core.repos.item_repo.dto.put_item_dto import PutItemDTO
+from HW_2.core.repos.item_repo.dto.patch_item_dto import PatchItemDTO
+from HW_2.core.entities.item import Item
+
+
+class ItemRepo(InterfaceItemRepo):
+    def __init__(self) -> None:
+        self._items = []
+
+    async def post_item(self, item_dto: PostItemDTO) -> Optional[Item]:
+        item = Item(id=len(self._items), name=item_dto.name,
+                    price=item_dto.price, deleted=False)
+        self._items.append(item)
+
+        return item
+
+    async def get_item_by_id(self, id: int) -> Optional[Item]:
+        try:
+            item = self._items[id]
+            return item
+        except IndexError:
+            return None
+
+    async def get_items(self,
+                        offset: Optional[int] = 0,
+                        limit: Optional[int] = 10,
+                        min_price: Optional[float] = None,
+                        max_price: Optional[float] = None,
+                        show_deleted: Optional[bool] = False) -> List[Item]:
+        items = self._items[offset:offset + limit]
+
+        if not show_deleted:
+            items = [item for item in items if not item.deleted]
+
+        if min_price is not None:
+            items = [item for item in items if item.price >= min_price]
+
+        if max_price is not None:
+            items = [item for item in items if item.price <= max_price]
+
+        return items
+
+    async def put_item(self, item_id: int,
+                       item_dto: PutItemDTO) -> Optional[Item]:
+        try:
+            replaced_item = self._items[item_id]
+        except IndexError:
+            return None
+
+        replaced_item.name = item_dto.name
+        replaced_item.price = item_dto.price
+
+        self._items[item_id] = replaced_item
+
+        return replaced_item
+
+    async def patch_item(self, item_id: int, item_dto: PatchItemDTO) -> Item:
+        try:
+            updated_item = self._items[item_id]
+        except IndexError:
+            return None
+
+        if item_dto.name is not None:
+            updated_item.name = item_dto.name
+
+        if item_dto.price is not None:
+            updated_item.price = item_dto.price
+
+        self._items[item_id] = updated_item
+
+        return updated_item
+
+    async def delete_item(self, item_id: int) -> Item:
+        try:
+            deleted_item = self._items[item_id]
+        except IndexError:
+            return None
+
+        deleted_item.deleted = True
+
+        self._items[item_id] = deleted_item
+
+        return deleted_item
